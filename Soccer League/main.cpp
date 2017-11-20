c82338f2c78dbbfb28386b667cdf01749ad52092
perkinhm <holden13652@aol.com>
2017-11-20 15:43:32 -0500

    ss

---
+++ Soccer League/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Soccer League
+//
+//  Created by Holden Perkins on 11/17/17.
+//  Copyright © 2017 Holden Perkins. All rights reserved.
+//
+
+#include <iostream>
+#include "player.h"
+int main() {
+    //do we need a buffer?
+    
+    return 0;
+}
